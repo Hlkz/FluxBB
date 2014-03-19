ada31affd98d46af88b4b117fb94be558a83d50e
Hlkz <hlkz@hotmail.fr>
2014-03-19 11:11:24 +0100

    base config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'HOST';
+$db_name = 'fluxbb';
+$db_username = 'USER';
+$db_password = 'PASS';
+$db_prefix = 'fluxbb_';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_RANDOM';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = 'RANDOM';
+
+define('PUN', 1);
